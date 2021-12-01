5d3e6a4b44dd5c23a13bf725739a08f6db4aa924
YuHwan Kim <khfbvxz@naver.com>
2021-12-02 01:03:23 +0900

    useState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import Wood from './wood';
+import { useState } from 'react';
+import Sub from './Sub';
 
 // 0. React 엔진 - 데이터 변경 감지에서 UI그려주는 !
 // 1. 실행과정 (index.html) -SPA
@@ -15,20 +16,38 @@ import Wood from './wood';
 //  - 내부에 적는 방법
 //  - 외부 파일에 적는 방법
 //  - 라이브러리 사용(부트스트랩, component-styled)
-let a = 10;
-const b = 20;
-function App() {
-  let c;
-  let d = undefined;
-  console.log(1, c);
 
-  const mystyle = {
-    color: 'red',
+function App() {
+  //let number = 1; // 상태 값 아님
+  const [num, setNum] = useState(5);
+  let sample = [
+    { id: 1, name: '홍길동' },
+    { id: 2, name: '임꺽정' },
+    { id: 3, name: '장보고' },
+    { id: 4, name: '코스' },
+  ];
+  console.log('App 실행됨 ');
+  const [users, setUsers] = useState(sample); //레퍼런스 변경되야 동작 !!
+
+  const download = () => {
+    //setUsers([...sample]);
+    //기존 데이터 세팅
+    // const a = sample.concat({ id: 5, name: '조자룡' });
+    // setUsers(a);
+
+    setUsers([...sample, { id: num, name: '조자룡' }]);
+    setNum(num + 1);
   };
+
+  // 랜더링 시점 = 상태값 변경
   return (
     <div>
-      <div>달</div>
-      <Wood />
+      <button onClick={download}>다운로드 </button>
+      {users.map((u) => (
+        <h1>
+          {u.id}, {u.name}
+        </h1>
+      ))}
     </div>
   );
 }
